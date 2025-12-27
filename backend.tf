132e8f7954af3c1f366a0c986e5b055ba77444ec
Althaf453 <althafshaik887@gmail.com>
2025-12-27 15:55:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraformdemo"
-    storage_account_name = "terraformdemostoraa990"
-    container_name       = "terraformcontain"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraform990"
+    container_name       = "tfstate"
     key                  = "default.terraform.tfstate"
     use_oidc             = true
   }
